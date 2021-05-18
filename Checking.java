97b5b319e0afbeb212ef7c3a80959f2c2a79328d
Jose Anthony Aguirre <79893435+JoseTonAguirre@users.noreply.github.com>
2021-05-18 15:27:05 +0800

    Update Checking

--- Checking.java
+++ Checking.java
@@ -1,2 +1,22 @@
-public class Checking {
+public class Checking extends Account
+{
+    public Checking(int accountNumber)
+    {
+        super(accountNumber);
+    }
+
+    public int getAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public String getAccountInfo()
+    {
+        return("Checking Account Information\nAcct Num: " + getAccountNumber() + "\nBalance: " + getBalance());
+    }
 }
